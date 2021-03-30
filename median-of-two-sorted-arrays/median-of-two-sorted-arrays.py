c48219ed4aac8ddb144fabdefb7785822adab1ff
Jesussilverioe <58699576+Jesussilverioe@users.noreply.github.com>
2021-03-30 03:58:45 -0500

    Time: 92 ms (68.13%), Space: 14.4 MB (80.63%) - LeetHub

---
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        margelist = nums1 + nums2
+        quo,remi = divmod(len(margelist),2)
+        if remi:
+            return sorted(margelist)[quo]
+        return sum(sorted(margelist)[quo -1: quo+1])/2
