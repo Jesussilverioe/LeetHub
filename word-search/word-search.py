0356dd283813915065576d3577098e6e26e06d4a
Jesussilverioe <58699576+Jesussilverioe@users.noreply.github.com>
2021-03-31 04:07:17 -0500

    Time: 3304 ms (40.31%), Space: 14.3 MB (97.77%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,27 +1,26 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         if len(board) == 0: return False
-
         row = len(board)
         col = len(board[0])
-        def explore(i,j, word):
+        
+        def bfs(i, j, word):
             if len(word) == 0:
                 return True
             temp = board[i][j]
             board[i][j] = '$'
-            neighbors = [(0,1),(0,-1),(1,0),(-1,0)]
-            
-            for x,y in neighbors:
-                newi = i+x
-                newj = j+y
-                if 0 <= newi < row and 0 <= newj < col and board[newi][newj] == word[0]:
-                    if explore(newi,newj, word[1:]):
+            neigbors = [[0,1],[0,-1],[1,0],[-1,0]]
+            for n1, n2 in neigbors:
+                ni = i+n1
+                nj = j+n2
+                if 0 <= ni < row and 0 <= nj < col and board[ni][nj] == word[0]:
+                    if bfs(ni,nj,word[1:]):
                         return True
             board[i][j] = temp
             return False
         
         for i in range(row):
             for j in range(col):
-                if board[i][j] == word[0] and explore(i,j, word[1:]):
+                if board[i][j] == word[0] and bfs(i,j, word[1:]):
                     return True
         return False
