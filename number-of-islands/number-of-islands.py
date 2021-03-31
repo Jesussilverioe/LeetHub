3933b9406577a44372813b6794be7b2c9e5ec600
Jesussilverioe <58699576+Jesussilverioe@users.noreply.github.com>
2021-03-30 20:30:31 -0500

    Time: 140 ms (67.58%), Space: 19.3 MB (13.86%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,31 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+        if not grid: return None
         row = len(grid)
         col = len(grid[0])
-        island = 0
         visited = set()
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c] == "1" and (r,c) not in visited:
-                    self.bfs(r,c,row,col,visited, grid)
+        island = 0
+        def bfs(i,j):
+            q = collections.deque()
+            q.append((i,j))
+            
+            neigbors = [[1,0],[-1,0],[0,1],[0,-1]]
+            visited.add((i,j))
+            while q:
+                point1,point2 = q.popleft()
+                for nr,nc in neigbors:
+                    np1 = point1 + nr
+                    np2 = point2 + nc
+                    if (0 <= np1 < row and 0 <= np2 < col and grid[np1][np2] == "1" and (np1,np2) not in visited):
+                        q.append((np1,np2))
+                        visited.add((np1,np2))
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == "1" and (i,j) not in visited:
+                    bfs(i,j)
                     island += 1
-        return island
-    
-    def bfs(self, r,c,row,col,visited, grid):
-        visited.add((r,c))
-        q = collections.deque()
-        q.append((r,c))
-        while q:
-            point1, point2 = q.popleft()
-            neighbors = [[0,1],[0,-1],[1,0],[-1,0]]
-            for n,nc in neighbors:
-                npoint1 = point1+n
-                npoint2 = point2+nc
-                if (npoint1 in range(row) and npoint2 in range(col) and grid[npoint1][npoint2] == "1"
-                    and (npoint1, npoint2) not in visited):
-                    visited.add((npoint1,npoint2))
-                    q.append((npoint1,npoint2))
                     
-        
+        return island
